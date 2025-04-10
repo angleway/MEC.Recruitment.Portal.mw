f77a989ac96f4feb40800126df6119bf67f7da04
angleway <donjoey44+mt@gmail.com>
2025-04-10 16:23:37 +0100

    Create mwp.js

---
+++ mwp.js
@@ -0,0 +1,6 @@
+
+var password = 'MEC'
+password=prompt('SMS :Takulandilani ku portal yolembera anthu, Chonde OK kuti muyambe ntchito yanu  ','MEC');
+if (password != 'MEC') {
+location.href='https://angleway.github.io/MEC.Recruitment.Portal.mw';
+}
